7e0b91b5cb9e18231590931bebd8611db6ed455e
Adrian Mayoral <mayoralito@gmail.com>
2014-10-27 10:01:05 -0600

    Initial Commit

---
+++ GameOfLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GameOfLife
+//
+//  Created by amayoral on 10/27/14.
+//  Copyright (c) 2014 adrianmayoral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GameOfLife/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GameOfLife
+//
+//  Created by amayoral on 10/27/14.
+//  Copyright (c) 2014 adrianmayoral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
