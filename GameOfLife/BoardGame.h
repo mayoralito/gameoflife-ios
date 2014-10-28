820c5996a444318145d078300e1a9e9e21d10758
amayoral <mayoralito@gmail.com>
2014-10-28 05:25:49 -0600

    Initial commit with contributors

---
+++ GameOfLife/BoardGame.h
@@ -0,0 +1,13 @@
+//
+//  BoardGame.h
+//  GameOfLife
+//
+//  Created by amayoral on 10/27/14.
+//  Copyright (c) 2014 adrianmayoral. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BoardGame : NSObject
+
+@end
---
+++ GameOfLife/BoardGame.m
@@ -0,0 +1,13 @@
+//
+//  BoardGame.m
+//  GameOfLife
+//
+//  Created by amayoral on 10/27/14.
+//  Copyright (c) 2014 adrianmayoral. All rights reserved.
+//
+
+#import "BoardGame.h"
+
+@implementation BoardGame
+
+@end
